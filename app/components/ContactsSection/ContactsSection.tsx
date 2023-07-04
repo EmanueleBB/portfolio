32466d762b84e438f0228486e4aa343c80ed1e91
EmanueleBB <121090087+EmanueleBB@users.noreply.github.com>
2023-07-04 10:14:34 +0200

    added first iteration of the styling of the contact form

--- app/components/ContactsSection/ContactsSection.tsx
+++ app/components/ContactsSection/ContactsSection.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import React from 'react'
+import React, { ChangeEvent, EventHandler, useState } from 'react'
 import styles from './contactsSection.module.css'
 import { useForm, SubmitHandler } from "react-hook-form"
 
@@ -22,23 +22,35 @@ const ContactsSection = () => {
    const onSubmit: SubmitHandler<Inputs> = (data) => console.log(data)
    console.log(watch("example")) // watch input value by passing the name of it
 
+   const [hasValue, setHasValue] = useState(false);
+
+   const handleInputChange = (e:ChangeEvent<HTMLInputElement>) => {
+      const inputValue = e.target.value;
+      setHasValue(inputValue !== '');
+      console.log(inputValue);
+   };
+
+
    return (
       <div className={styles.mainContainer}>
          <h2>
             Let's get in touch!<span>😎</span>
          </h2>
 
-         <form onSubmit={handleSubmit(onSubmit)}>
-      
-      <input defaultValue="test" {...register("example")} />
+         <form onSubmit={handleSubmit(onSubmit)} className={styles.contactForm}>
+
+            <div className={`${styles.formGroup} ${hasValue ? styles.hasValue : ''}`}>
+               <input defaultValue="test" {...register("example")} id='ciao' onChange={handleInputChange}/>
+               <label htmlFor="ciao">Bellazio</label>
+            </div>
 
      
-      <input {...register("exampleRequired", { required: true })} />
-      {/* errors will return when field validation fails  */}
-      {errors.exampleRequired && <span>This field is required</span>}
+            <input {...register("exampleRequired", { required: true })} />
+               {/* errors will return when field validation fails  */}
+            {errors.exampleRequired && <span>This field is required</span>}
 
-      <input type="submit" />
-    </form>
+            <button type="submit">Invia</button>
+         </form>
          
       </div>
    )
