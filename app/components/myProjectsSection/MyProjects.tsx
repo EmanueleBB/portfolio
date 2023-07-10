7a32185e98a6ee51a502f8d52a95e1ffdbdf094b
EmanueleBB <121090087+EmanueleBB@users.noreply.github.com>
2023-07-10 17:42:57 +0200

    started implementation of the projects 3d animation

--- app/components/myProjectsSection/MyProjects.tsx
+++ app/components/myProjectsSection/MyProjects.tsx
@@ -13,31 +13,37 @@ const MyProjects = () => {
                title='Full stack Airbnb Clone'
                imgSrc=''
                stack={['Next JS 13.3','Mongo DB', 'Prisma', 'Next Auth', 'Cloudinary CDN', 'Tailwind CSS']}
+               description='An Airbnb clone made with NextJS 13.3'
+            />
+            <ProjectCard
+               title='Audi RS6 Redesign'
+               imgSrc=''
+               stack={['Next JS 13.3','Photoshop', 'Sass', 'Gsap']}
+               description='Because beautiful cars deserve beautiful landing pages'
             />
             <ProjectCard
                title='Full stack Airbnb Clone'
                imgSrc=''
-               stack={['Next JS 13.3','Mongo DB', 'Prisma', 'Next Auth', 'Cloudinary CDN', 'Tailwind CSS']}
-            />
-            <ProjectCard
-               title='Full stack Airbnb Clone'
-               imgSrc=''
-               stack={['Next JS 13.3','Mongo DB', 'Prisma', 'Next Auth', 'Cloudinary CDN', 'Tailwind CSS']}
+               stack={['HTML','CSS','Javascript']}
+               description='My first real project, made with plain HTML, CSS and Javascript'
             />
             <ProjectCard
             title='Full stack Airbnb Clone'
             imgSrc=''
             stack={['Next JS 13.3','Mongo DB', 'Prisma', 'Next Auth', 'Cloudinary CDN', 'Tailwind CSS']}
+            description=''
             />
             <ProjectCard
             title='Full stack Airbnb Clone'
             imgSrc=''
             stack={['Next JS 13.3','Mongo DB', 'Prisma', 'Next Auth', 'Cloudinary CDN', 'Tailwind CSS']}
+            description=''
             />
             <ProjectCard
             title='Full stack Airbnb Clone'
             imgSrc=''
             stack={['Next JS 13.3','Mongo DB', 'Prisma', 'Next Auth', 'Cloudinary CDN', 'Tailwind CSS']}
+            description=''
             />
          </div>
       </div>
--- app/components/myProjectsSection/ProjectCard.tsx
+++ app/components/myProjectsSection/ProjectCard.tsx
@@ -1,11 +1,15 @@
+'use client'
+
+import gsap from 'gsap';
 import Image from 'next/image';
-import React from 'react';
+import React, { useRef } from 'react';
 import styles from './projects.module.css'
 
 interface ProjectCardProps{
    title:string;
    imgSrc:string;
    stack:string[];
+   description:string;
 }
 
 const ProjectCard:React.FC<ProjectCardProps> = ({
@@ -14,12 +18,47 @@ const ProjectCard:React.FC<ProjectCardProps> = ({
    stack
 }) => {
 
+   const contentRef=useRef<HTMLDivElement>(null);
+
+
+
+   const animateContent = () => {
+      const tl=gsap.timeline();
+      tl.to(contentRef.current,{
+         y:-200,
+         duration:0.5,
+         ease:'power2.out',
+         transform:'perspective(1000px) rotateX(-30deg) scaleY(1.6)',
+         transformOrigin: 'bottom',
+      }).to(contentRef.current,{
+         duration:0.5,
+         ease:'power2.inOut',
+         y:0,
+         transform:'scaleY(1)',
+         transformOrigin: 'top',
+         zIndex:10,
+      }).to(contentRef.current,{
+         y:0,
+         duration:0.2,
+         transform:'scale(2)',
+         ease:'power2.in',
+         
+      })
+   }
+
    return (
       <div className={styles.projectCardContainer}>
          <Image fill src={imgSrc} alt='Image of the project'/>
          <h4>
             {title}
          </h4>
+         <div className={styles.backSide}>
+            <div className={styles.content} ref={contentRef}>
+            </div>
+            <div className={styles.frontSide} onClick={animateContent}>
+            </div>
+         </div>
+         
       </div>
    )
 }
