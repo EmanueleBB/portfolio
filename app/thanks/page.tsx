9e73c3643daa82251fc11dc3ef299bbfbae7a2ef
EmanueleBB <121090087+EmanueleBB@users.noreply.github.com>
2023-07-26 12:22:52 +0200

    completed the thanks page

--- app/thanks/page.tsx
+++ app/thanks/page.tsx
@@ -1,13 +1,21 @@
 import Link from 'next/link'
 import React from 'react'
+import styles from './thanks.module.css'
 
-const page = () => {
+const Thanks = () => {
    return (
-      <div>Well, thank you for contacting me. I&apos;ll make sure to answer as soon as possible. 
-         <Link href='/'>Go home bruh</Link>
+      <div className={styles.mainContanier}>
+         <h1>
+            Thank you for reaching out!! 😄
+         </h1>
+         <p>
+            
+            I&apos;ll do my best to get back to you as soon as possible. If I do not respond within 2 business days, try to contact me via some of the other contacts in the contacts section or at @manu.burratti@gmail.com  
+         </p> 
+         <Link href='/' className={styles.backHomeLink}>Go back home</Link>
       </div>
 
    )
    }
 
-export default page
+export default Thanks;
