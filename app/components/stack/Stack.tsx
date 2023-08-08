98ebc172f5177922503a23b741c7f1d35331b788
EmanueleBB <121090087+EmanueleBB@users.noreply.github.com>
2023-08-08 17:11:05 +0200

    modified intro div in the stack section

--- app/components/stack/Stack.tsx
+++ app/components/stack/Stack.tsx
@@ -30,6 +30,9 @@ const Stack = () => {
    const horizontalWrapperRef = useRef<HTMLDivElement>(null);
    const progressBarAndCloseDivRef = useRef<HTMLDivElement>(null);
    const paragraphRef=useRef<HTMLParagraphElement>(null);
+   const arrowRef=useRef<SVGSVGElement>(null);
+   
+
    const {windowHeight,windowWidth,maxDimension,maxDimensionValue, minDimensionValue} = useWindowSizeInfos();
 
    const { toggleIsVisible } = useNavbarStore();
@@ -49,7 +52,6 @@ const Stack = () => {
    //For the progressBar we need to know how much of the content has been scrolled
    useEffect(() => {
 
-      const horizontalWrapperRefValue=horizontalWrapperRef.current;
 
       const updatePercentageScrolled = () => {
          if (horizontalWrapperRef.current) {
@@ -133,13 +135,24 @@ const Stack = () => {
                opacity: 1,
                duration: 1.2,
                ease:'power1.out',
-               stagger: 0.05,
-            },'<');
+               stagger: 0.06,
+            },'<').to(arrowRef.current,{
+               top:0,
+               rotate:'-720',
+               ease:'power3.out',
+               opacity:0.6,
+               duration:1.3,
+               onComplete:()=>{
+                  gsap.to(`.${styles.scrollRightSpan}`,{
+                     opacity:1,
+                  })
+               }
+            });
          }
       }
-    }, [paragraphIsAnimated]);
+   }, [paragraphIsAnimated]);
 
-   
+  
 
  
    const handleClick = () => {
@@ -150,15 +163,23 @@ const Stack = () => {
    return (  
       <HorizontalWrapper ref={horizontalWrapperRef}>
          <div className={`${styles.introDiv} ${styles.stackDiv}`}>
-            {/* <Image
-               src='/leonardoNotes.png'
-               alt='a cool image'
-               fill
-               style={{objectFit: "cover",opacity: "0.1"}}
-            /> */}
+      
             <p className={`${styles.generalP} ${styles.introP}`} ref={paragraphRef}>
                {divideInSpans("This is a somewhat detailed list of all the tech I've gained experience with in the last years. It covers the main tools I use from the design stage to the front-end / back-end development.")}
             </p>
+            <svg ref={arrowRef} width="51" height="46" viewBox="0 0 51 46" fill="none" xmlns="http://www.w3.org/2000/svg">
+               <defs>
+                  <linearGradient id="myGradient" x1="0%" y1="0%" x2="100%" y2="0%">
+                     <stop offset="50%" stopColor="white" />
+                     <stop offset="100%" stopColor="#b6b6b6" />
+                  </linearGradient>
+               </defs>
+            
+               <path fill="url(#myGradient)" d="M3 20C1.34315 20 1.44847e-07 21.3431 0 23C-1.44846e-07 24.6569 1.34315 26 3 26L3 20ZM50.1213 25.1213C51.2929 23.9498 51.2929 22.0503 50.1213 20.8787L31.0294 1.7868C29.8579 0.615226 27.9584 0.615226 26.7868 1.7868C25.6152 2.95837 25.6152 4.85787 26.7868 6.02944L43.7574 23L26.7868 39.9706C25.6152 41.1421 25.6152 43.0416 26.7868 44.2132C27.9584 45.3848 29.8579 45.3848 31.0294 44.2132L50.1213 25.1213ZM3 26L48 26L48 20L3 20L3 26Z" />
+               
+            </svg>
+            <span className={styles.scrollRightSpan} >Please proceed sideway</span>
+
          </div>
          <FigmaDiv/>  
          <PhotoshopDiv/>
@@ -178,4 +199,4 @@ const Stack = () => {
    );
 };
  
- export default Stack;
+export default Stack;
