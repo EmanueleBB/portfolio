eba2f85778b7fa2b93418e27933e882281cdc526
EmanueleBB <121090087+EmanueleBB@users.noreply.github.com>
2023-08-20 15:42:54 +0200

    modified import for ContactSection

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 import CardSection from "./components/cardsSection/CardSection";
 import CodeSection from "./components/codeSection/CodeSection";
-import ContactsSection from "./components/contactsSection/ContactsSection";
+
 import FirstText from "./components/firstText/FirstText";
 import Footer from "./components/footer/Footer";
 import HeroSection from "./components/hero/HeroSection";
@@ -9,6 +9,7 @@ import MainContainer from "./components/mainContainer/MainContainer";
 import MyProjects from "./components/myProjectsSection/MyProjects";
 import Navbar from "./components/navbar/Navbar";
 import Stack from "./components/stack/Stack";
+import ContactsSection from "./components/contactsSection/ContactsSection";
 
 export default function Home(){
 	return (
